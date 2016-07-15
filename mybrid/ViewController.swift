2ef20d6b598e0181c9612c80a1d20f2330cce370
Thomas Liu <liujiachengzhenniu@qq.com>
2016-07-15 20:38:20 +0800

    Initial Commit

---
+++ mybrid/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mybrid
+//
+//  Created by Thomas Liu on 16/7/15.
+//  Copyright © 2016年 Thomas Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
